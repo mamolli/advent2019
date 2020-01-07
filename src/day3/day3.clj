d5fa4c4381103b768b48f5649aec2719641fdc25
mamolli <roguas@gmail.com>
2020-01-07 01:32:11 +0100

    Day 3 part 2

--- src/day3/day3.clj
+++ src/day3/day3.clj
@@ -3,6 +3,11 @@
   (:require [clojure.set :as set])
   (:require [clojure.math.numeric-tower :as math]))
 
+;PART 1
+
+(def initial [0 0])
+(def directions {"R" [0 identity], "L" [0 -], "U" [1 identity], "D" [1 -]})
+
 (defn get-input
   "Get codes from path"
   [path]
@@ -22,9 +27,9 @@
   "Apply delta instruction and return vector of 2-point vectors."
   ; there should be some checking if points are forming a line
   [startp delta]
-  (mapv + startp delta))
+  (map + startp delta))
 
-(defn dline [startp delta]
+(defn pline [startp delta]
   "Take a start and endpoint(2-elem vectors) and return a line as vector of vectors(2-elem vectors)"
   (for 
     [x (irange (first startp) (first (apply-delta startp delta)))
@@ -38,9 +43,10 @@
          deltas delta-points]
     (if (empty? deltas)
       all-points
-      (recur
-        (lazy-cat all-points (dline (last all-points) (first deltas)))
-        (drop 1 deltas)))))
+      (let [last-point (last all-points)]
+        (recur
+          (lazy-cat all-points (rest (pline last-point (first deltas))))
+          (drop 1 deltas))))))
 
 (defn split-delta-ins
   "Convert instruction into 2-point delta vector."
@@ -51,20 +57,27 @@
         [axis operation] (directions direction)]          
     (assoc initial axis (operation distance))))
 
-
-
-(def initial [0 0])
-(def directions {"R" [0 identity], "L" [0 -], "U" [1 identity], "D" [1 -]})
-(def cables-data (mapv #(str/split % #",") (get-input "src/day3/input-data.txt")))
-(def cables-delta-data (mapv #(mapv split-delta-ins %) cables-data))
+(def cables-data (map #(str/split % #",") (get-input "src/day3/input-data.txt")))
+(def cables-delta-data (map #(map split-delta-ins %) cables-data))
 
 ; result data
-(def reduced-cables (map #(set (reduce-points %)) cables-delta-data))
-(def intersection (disj (apply set/intersection reduced-cables) initial))
+(def reduced-cables (map #(reduce-points %) cables-delta-data))
+(def intersections (disj (apply set/intersection (mapv set reduced-cables)) initial))
 ;clostest helper = abs +
 (defn abs-distance [point]
+  "Calculate distance by adding absolutes of points"
   (+ (math/abs (first point)) (math/abs (second point))))
 
-(def closest-point (first (sort #(compare (abs-distance %1) (abs-distance %2)) intersection)))
+(def closest-point (first (sort #(compare (abs-distance %1) (abs-distance %2)) intersections)))
 (def closest-point-distance (abs-distance closest-point))
-; from [0 0]
+
+; PART 2
+; i wont comment on clojure's lack of index-of
+(defn indexes-of [el coll] (keep-indexed #(if (= el %2) %1) coll)) 
+(defn index-of [el coll]
+  (first (indexes-of el coll)))
+
+; TODO: ordering is fucked
+(apply min
+  (map #(+ (first %) (second %))
+    (map (fn [intersection] (map (fn [cable] (index-of intersection cable)) reduced-cables)) intersections)))
